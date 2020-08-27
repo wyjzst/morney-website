1fca5c08666e26a303c8dc38b81b2be7c577a5cd
wyjzst <wyjzst@126.com>
2020-08-27 12:13:08 +0800

    deploy

---
+++ precache-manifest.224d9731f499638f29b36ab6782c3a9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8ff8d31555cfb46a94e",
+    "url": "/morney-website/css/app.22c3fc3c.css"
+  },
+  {
+    "revision": "b588a373c00f827957eb69c7b8a18058",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "f8ff8d31555cfb46a94e",
+    "url": "/morney-website/js/app.2b3e45eb.js"
+  },
+  {
+    "revision": "84e548bd816f0123f3d8",
+    "url": "/morney-website/js/chunk-vendors.e38d596b.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
